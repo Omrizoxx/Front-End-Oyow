a185329dd23f0404e76fe6a779643d1cbc37507d
Omari <omaradan091@gmail.com>
2025-09-23 17:26:16 +0300

    Ready for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 3000,
+    port: 8080,
+    allowedHosts: ["front-end-oyow-oro0.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
